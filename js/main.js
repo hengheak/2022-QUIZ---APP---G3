4b08a594c9bcdaae98c3c2c75543f642ee7c52b8
Student <student@PNCL019.pnc.lan>
2022-01-29 10:20:36 +0700

    add question and display question

--- js/main.js
+++ js/main.js
@@ -1,26 +1,89 @@
-//menu########### -heng-
-let getQuiz = document.getElementById("play-quiz");
-let getHide = document.getElementById("hide");
-getQuiz.style.display="none";
-function playQuiz(e){
-  e.preventDefault()
-  getQuiz.style.display="block";
-  getHide.style.display="none";
+function makers(){
+  let quest=document.querySelector(".optional");
+  
+    let tion1=document.querySelector("#tion1");
+ 
+    let tion2=document.querySelector("#tion2");
+    
+    let tion3=document.querySelector("#tion3");
 
-}
-let btnShow= document.getElementById("btn-quiz");
-btnShow.addEventListener("click",playQuiz)
-// getQuiz.style.display="none";
-function edit(e){
-  e.preventDefault()
-  getQuiz.style.display="none";
-  getHide.style.display="block";
+    let tion4 = document.querySelector("#tion4");
+    
 
+    // app the question and display to browswer
+    let object=[{Question:quest.value,answers1:tion1.value,answers2:tion2.value,answers3:tion3.value,answers4:tion4.value}];
+    localStorage.setItem("Narong",JSON.stringify( object));
+
+
+    
+    for (let value of object){
+      let bigquestion=document.createElement("form");
+      let storage1=document.querySelector(".biganswer");
+      storage1.appendChild(bigquestion);
+      bigquestion.textContent=value.Question;
+      
+      
+      let makestor=document.querySelector(".spaners");
+      storage1.appendChild(makestor);
+
+
+      // Store only two question to span=============================
+
+    
+      let show1=document.createElement("span");
+      show1.textContent=value.answers1;
+      makestor.appendChild(show1);
+
+
+
+      let show2=document.createElement("span");
+      show2.textContent=value.answers2;
+      makestor.appendChild(show2);
+      
+      let show3=document.createElement("span");
+      show3.textContent=value.answers3;
+      makestor.appendChild(show3);
+      
+      let show4=document.createElement("span");
+      show4.textContent=value.answers4;
+      makestor.appendChild(show4);
+      
+      console.log(makestor)
+
+      // Reloat the name input when we aready input
+      quest.value="";
+      tion1.value="";
+      tion2.value="";
+      tion3.value="";
+      tion4.value="";
+    };
+  };
+  let creaters=document.querySelector(".addtogo");
+  creaters.addEventListener("click",makers);
+  
+  
+  // ================ hide and show the quiz and make it done ==========>>>>>>>.
+
+  let showImage = () => {
+    // your code here
+    let image1 = document.querySelectorAll('.addition_quiz');
+    for (let pic of image1){
+        pic.style.display = 'block';
+    }
 }
-let btnHide= document.getElementById("btn-edit-quiz");
-btnHide.addEventListener("click",edit)
-const question = [
-  {question:'How old are you ?',
-answer: [{text: '18 year old', isAnswer:false}, {text:'20 year old', isAnswer:false},{text:'24 year old', isAnswer:false},{text:'23 year old', isAnswer:true}]}
-]
- let firsQuestion=0;
+let hideImage = () => {
+   // your code here
+   let image2 = document.querySelectorAll('.addition_quiz');
+   for (let pic of image2){
+       pic.style.display = 'none';
+   };
+};
+
+let btnShow = document.querySelector('#getback');
+
+let btnHide = document.querySelector('#getdone');
+
+btnHide.addEventListener('click', hideImage);
+btnShow.addEventListener('click', showImage);
+  
+  
